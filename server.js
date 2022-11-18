792e51c7318bd77acb9c4bef96e3f85ed447c3f3
Vikki <anexcruise@gmail.com>
2022-11-18 22:19:36 +0530

    updated with cherrio

--- server.js
+++ server.js
@@ -1,12 +1,29 @@
-const jsonServer = require('json-server');
-const server = jsonServer.create();
+const express = require('express');
+const axios = require('axios')
+const cheerio = require('cheerio')
+const app = express()
+const port = 3500;
+const articles = []
+app.use(express.json())
+app.get('/', (req,res)=>{
+    res.json(`welcome to fifa 2022`)
+})
+app.get('/match',(req,res)=>{
+    axios
+      .get('https://en.wikipedia.org/wiki/2022_FIFA_World_Cup#Round_of_16')
+      .then((response) => {
+       const html = response.data;
+       const $ = cheerio.load(html);
+       $('.footballbox', html).each(function () {
+         const match = $(this, '.fdate').text();
+         articles.push({ match });
+       });
+       res.json(articles)
+      });
+      
+})
+ 
 
-const router = jsonServer.router('db.json');
-
-const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3500;
-
-server.use(middlewares);
-server.use(router)
-
-server.listen(port)
+app.listen(port,()=>{
+    console.log(`listening to the port: ${port}`)
+})
